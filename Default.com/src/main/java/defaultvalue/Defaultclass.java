f3996e583be4c6b71dc076d3c893c0f310559e71
abul-azad <107379552+abul-azad@users.noreply.github.com>
2022-08-28 21:30:32 -0400

    Default.com This is done by Azad.

---
+++ Default.com/src/main/java/defaultvalue/Defaultclass.java
@@ -0,0 +1,38 @@
+package defaultvalue;
+
+public class Defaultclass 
+   {//start from here
+	static int a;
+	static double b;
+	static String c;
+	static Boolean d;
+	
+	public static void main(String[] args) 
+	{
+	
+
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	}
+
+public int getmoney() {
+	return 0;
+	
+}
+
+public double getpassword() {
+	return 0;
+	
+}
+
+
+}//end here		
+	
+
+
+	
+	
+
+   
